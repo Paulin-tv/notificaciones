e5d7cde8b4518d4e5ce6a55f7b917306d8ef50ab
paulin-tv <paula_bcn13@hotmail.com>
2023-07-04 17:37:42 +0200

    commit 4/7

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,12 +1,23 @@
-const authUser = (permissions) => {
+const authAdmin = (permissions) => {
     return (req, res, next) => {
         const userRole = req.body.role
         if (permissions.includes(userRole)){
             next()
         } else {
-            return res.status(401).json("Insufficient permits.")
+            return res.status(401).json("Insufficient admin permits.")
         };
     };
 };
 
-module.exports = {authUser}
+const authUser = () => {
+    return (req, res, next) => {
+        const userId = req.params.userId;
+        if (userId === req.body.userId) {
+            next();
+        } else {
+            return res.status(401).json("Insufficient permits.");
+        };
+    };
+};
+
+module.exports = { authAdmin, authUser };
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const knex = require("../db/knex");
 const cors = require ("cors");
 const PORT = 5000;
-const {authUser} = require('./middlewares.js')
+const {authUser, authUserr, authAdmin} = require('./middlewares.js')
 
 
 //parsing the req to a json body. (middleware)
@@ -32,7 +32,8 @@ app.get("/test", (req, res) => {
 //#API REQUESTS OR ENDPOINTS
 
 //This function checks if database tasks exists. If so, it will post the new task object. Else, it will create the table and then call the 'createTask function to post.
-app.post("/tasks", authUser(["admin"]), async (req, res) => {
+//Admin permit doesnt work when passed on postman due to table not containing a value field.Will assign userId 1 to admin upon creation. 
+app.post("/tasks", authUser(["1"]), async (req, res) => {
     try {
         const tableExists = await knex.schema.hasTable("tasks");
         if (tableExists) {
@@ -55,13 +56,15 @@ app.post("/tasks", authUser(["admin"]), async (req, res) => {
 });
 
 //Retrieves all existing tasks in table regardless of userId
-app.get("/tasks", async (req, res) => {
+//Admin permit works
+app.get("/tasks", authAdmin(["admin"]), async (req, res) => {
     const tasks = await db.getAllTasks();
     res.status(200).json({tasks});
 });
 
 //Retrieves task by individual id, this will serve upon clicking on it and being able to edit them
-app.get("/tasks/:id", async (req, res) => {
+//permits for admin work fine when "role":"admin" is passed
+app.get("/tasks/:id", authAdmin(["admin"]), async (req, res) => {
     try {   const task = await db.getTaskById(req.params.id);
             res.status(200).json(task);
        
@@ -73,17 +76,17 @@ app.get("/tasks/:id", async (req, res) => {
 
 
 // put request to update existing tasks
-app.put("/tasks/:id", async (req, res) => {
+app.put("/tasks/:id", authAdmin(["admin"]), async (req, res) => {
     try {
         const id = await db.updateTask(req.params.id, req.body);
         res.status(200).json({id});
     } catch (err) {
-     res.send(error)
+     res.send(error).json({error: "error"})
     }
 });
 
 //Delete request of individual task
-app.delete("/tasks/:id", async (req, res) => {
+app.delete("/tasks/:id", authAdmin(["admin"]), async (req, res) => {
     try {
             await db.deleteTask(req.params.id);
             res.status(200).json({success: true});
@@ -94,17 +97,20 @@ app.delete("/tasks/:id", async (req, res) => {
     }
 });
 
+
+//# USER ENDPOINTS
+
 //Retrieves all tasks by userId, used to display the array of tasks in the feed filtered by userId.
-app.get("/tasks/:userId", async (req, res) => {
+app.get("/main/:userId", authUser(), async (req, res) => {
     try {
         const tasks = await db.getTasksByUserId(req.params.userId);
+        console.log(tasks); // log the tasks array to the console
         res.status(200).json({tasks});
     } catch (err) {
         console.error(err);
         res.status(500).json({error: "Internal Server Error"});
     }
 });
-
 
 
 app.listen(PORT, () => console.log("Server is running on port 5000."));
